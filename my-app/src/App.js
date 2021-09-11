40762e25dedbde24395f7779a85555ee66795978
draganajotic <draganajotic6@gmail.com>
2021-09-11 11:36:00 +0200

    Access Token resen

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,15 @@
 import { Fragment } from 'react';
 import './App.css';
+import {useHistory} from 'react-router-dom'
 
-import { LoginForm } from './components/Login/Login';
+import LoginForm  from './components/Login/Login';
 
 function App() {
+  let history = useHistory();
+  if(!localStorage.getItem("token")) {
+    //history.push("/login");
+  }
+  
   return (
     <Fragment>
       <LoginForm />
---
+++ my-app/src/components/Candidates/Candidates.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
+
+export function Candidates () {
+    return (
+        <div className="login">
+            <Header title="Login Page" showItems={true}/>
+            <Footer/>
+
+        </div>
+    )
+}
